3db99b14d7034e34b9c8a9c42696a24befc58dfd
Manassarn Manoonchai <manassarn@gmail.com>
2020-11-01 23:38:08 +0700

    chore(deps): replace tslint with eslint

--- src/test/suite/extension.test.ts
+++ src/test/suite/extension.test.ts
@@ -1,89 +1,89 @@
-import * as assert from 'assert';
-import { before } from 'mocha';
-import { stub } from 'sinon';
-import * as fs from 'fs';
-import * as path from 'path';
+import * as assert from "assert";
+import { before } from "mocha";
+import { stub } from "sinon";
+import * as fs from "fs";
+import * as path from "path";
 
 // You can import and use all API from the 'vscode' module
 // as well as import your extension to test it
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 // import * as myExtension from '../extension';
 
-const workspacePath = path.resolve(__dirname, '../../../src/test/fixtures/workspace');
+const workspacePath = path.resolve(
+  __dirname,
+  "../../../src/test/fixtures/workspace"
+);
 
-suite('Extension Test Suite', () => {
-	before(() => {
-		vscode.window.showInformationMessage('Start all tests.');
-	});
+suite("Extension Test Suite", () => {
+  before(() => {
+    vscode.window.showInformationMessage("Start all tests.");
+  });
 
-	test('Create & open files', async () => {
-    let files:string[] = [
-      "hello.txt",
-      "hello_test.txt",
-    ];
+  test("Create & open files", async () => {
+    let files: string[] = ["hello.txt", "hello_test.txt"];
 
-    let mappings:string[][] = [
-      [
-        "*.txt",
-        "*_test.txt",
-      ]
-    ];
+    let mappings: string[][] = [["*.txt", "*_test.txt"]];
 
     for (let file of files) {
-      fs.closeSync(fs.openSync(`${workspacePath}/${file}`, 'w'));
+      fs.closeSync(fs.openSync(`${workspacePath}/${file}`, "w"));
     }
 
     // Update config
     let target = vscode.ConfigurationTarget.Workspace;
-    await vscode.workspace.getConfiguration('switcheroo').update('mappings', mappings, target);
+    await vscode.workspace
+      .getConfiguration("switcheroo")
+      .update("mappings", mappings, target);
 
     // Open file
-    let doc = await vscode
-      .workspace
-      .openTextDocument(`${workspacePath}/${files[0]}`);
+    let doc = await vscode.workspace.openTextDocument(
+      `${workspacePath}/${files[0]}`
+    );
 
     await vscode.window.showTextDocument(doc);
 
     // Swap to other file
-    await vscode.commands.executeCommand('extension.switcheroo.swap');
+    await vscode.commands.executeCommand("extension.switcheroo.swap");
 
     // Expect another file
-    assert(vscode.window.activeTextEditor!.document.fileName === `${workspacePath}/${files[1]}`);
+    assert(
+      vscode.window.activeTextEditor!.document.fileName ===
+        `${workspacePath}/${files[1]}`
+    );
   }).timeout(5000);
 
-  test('Prompt to create file if counterpart not exists', async() => {
-    let files:string[] = [
-      "hello.txt",
-    ];
+  test("Prompt to create file if counterpart not exists", async () => {
+    let files: string[] = ["hello.txt"];
 
-    let mappings:string[][] = [
+    let mappings: string[][] = [
       [
         "hello.txt",
         "hello_test_tmp.txt", // File to be created
-      ]
+      ],
     ];
 
     for (let file of files) {
-      fs.closeSync(fs.openSync(`${workspacePath}/${file}`, 'w'));
+      fs.closeSync(fs.openSync(`${workspacePath}/${file}`, "w"));
     }
 
     // Update config
     let target = vscode.ConfigurationTarget.Workspace;
-    await vscode.workspace.getConfiguration('switcheroo').update('mappings', mappings, target);
+    await vscode.workspace
+      .getConfiguration("switcheroo")
+      .update("mappings", mappings, target);
 
     // Open file
-    let doc = await vscode
-    .workspace
-    .openTextDocument(`${workspacePath}/${files[0]}`);
+    let doc = await vscode.workspace.openTextDocument(
+      `${workspacePath}/${files[0]}`
+    );
 
     await vscode.window.showTextDocument(doc);
 
     // Expect vscode.window.showQuickPick and choose Yes
     let quickPickStub = stub(vscode.window, "showQuickPick");
-    quickPickStub.resolves({ label: 'Yes' });
+    quickPickStub.resolves({ label: "Yes" });
 
     // Swap to other file
-    await vscode.commands.executeCommand('extension.switcheroo.swap');
+    await vscode.commands.executeCommand("extension.switcheroo.swap");
 
     assert(fs.existsSync(`${workspacePath}/hello_test_tmp.txt`));
 
